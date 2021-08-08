bcaaa722ceac19d0a3a11e4d0fbfd75e55b42f02
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2021-08-08 10:15:38 +0200

    Split Clojure functionality
    The function mapper is now using 'clj' as a prefix. It provides two
    functions, now: 'delegate' and 'function'

--- plugin/src/main/java/com/lambdaschmiede/camunda/clojure/ClojureFunctionMapper.java
+++ plugin/src/main/java/com/lambdaschmiede/camunda/clojure/ClojureFunctionMapper.java
@@ -1,47 +1,88 @@
 package com.lambdaschmiede.camunda.clojure;
 
 import clojure.java.api.Clojure;
+import clojure.lang.IFn;
+
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.impl.el.CommandContextFunctionMapper;
 import org.camunda.bpm.engine.impl.javax.el.FunctionMapper;
 import org.camunda.bpm.engine.impl.util.ReflectUtil;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * A Function mapper which provides the 'clj(arg)' function to Camundas UEL context.
+ * A Function mapper which provides the 'clj(arg)' function to Camundas UEL
+ * context.
  *
- * The argument is a string, referencing a Clojure function with its qualified name.
- * The targeted function has to accept exactly one parameter. This parameter will
- * be passed by the engine as an instance of DelegateExecution.
+ * The argument is a string, referencing a Clojure function with its qualified
+ * name. The targeted function has to accept exactly one parameter. This
+ * parameter will be passed by the engine as an instance of DelegateExecution.
  */
 public class ClojureFunctionMapper extends FunctionMapper {
 
+    public static Map<String, Method> FUNCTION_MAP = new HashMap<>();
+
     /**
-     * Camunda will query this method on a list of registered FunctionMapper instances
-     * with a UEL expression until the first one returns a value. This function checks
-     * if the localName of the UEL expression is called 'clj'. If so, it returns a reference
-     * to the static method 'eval' in this same class.
+     * Camunda will query this method on a list of registered FunctionMapper
+     * instances with a UEL expression until the first one returns a value. This
+     * function checks if the localName of the UEL expression is called 'clj'. If
+     * so, it returns a reference to the static method 'eval' in this same class.
      *
-     * If the localName does not match, it returns null and Camunda will query the next
-     * FunctionMapper instance in its list
+     * If the localName does not match, it returns null and Camunda will query the
+     * next FunctionMapper instance in its list
      */
     @Override
     public Method resolveFunction(String prefix, String localName) {
-        if ("clj".equalsIgnoreCase(localName)) {
-            return ReflectUtil.getMethod(ClojureFunctionMapper.class, "eval", String.class);
-        } else {
+        if (!"clj".equals(prefix)) {
             return null;
+        }
+        initializeFunctionMapIfEmpty();
+        return FUNCTION_MAP.get(localName);
+    }
+
+    private void initializeFunctionMapIfEmpty() {
+        if (FUNCTION_MAP.isEmpty()) {
+            synchronized (CommandContextFunctionMapper.class) {
+                if (FUNCTION_MAP.isEmpty()) {
+                    FUNCTION_MAP.put("delegate",
+                            ReflectUtil.getMethod(ClojureFunctionMapper.class, "delegate", String.class));
+                    FUNCTION_MAP.put("function",
+                            ReflectUtil.getMethod(ClojureFunctionMapper.class, "function", String.class, Object[].class));
+                }
+            }
         }
     }
 
     /**
      * Receives the expression which is passed as a parameter in the 'clj' function.
      * E.g. if the expression is `clj('namespace/function')`, the parameter will
-     * contain the value 'namespace/function'. This method creates a new instance
-     * of JavaDelegate on the spot, with a body that invokes the referenced Clojure
+     * contain the value 'namespace/function'. This method creates a new instance of
+     * JavaDelegate on the spot, with a body that invokes the referenced Clojure
      * function with its DelegateExecution parameter passed as an argument.
      */
-    public static JavaDelegate eval(String expression) {
+    public static JavaDelegate delegate(String expression) {
         return execution -> Clojure.var(expression).invoke(execution);
+    }
+
+    public static Object function(String expression, Object... params) {
+        int paramSize = params.length;
+        IFn fun = Clojure.var(expression);
+        switch (paramSize) {
+            case 0:
+                return fun.invoke();
+            case 1:
+                return fun.invoke(params[0]);
+            case 2:
+                return fun.invoke(params[0], params[1]);
+            case 3:
+                return fun.invoke(params[0], params[1], params[2]);
+            // TODO: Some more params would be nice, too
+            default:
+                throw new IllegalArgumentException(
+                        String.format("Parameter size of %s is not allowed for a Clojure expression", paramSize));
+        }
+
     }
 }
--- test/src/test/java/com/lambdaschmiede/camunda/clojure/ProcessEngineTest.java
+++ test/src/test/java/com/lambdaschmiede/camunda/clojure/ProcessEngineTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 
@@ -22,12 +23,23 @@ public class ProcessEngineTest {
 
     @Test
     @Deployment(resources = "test-process.bpmn")
-    public void testHappyPath() {
+    public void testPositiveValue() {
         IFn func = Clojure.var("clojure.core", "load-file");
         func.invoke("delegate.clj");
-        ProcessInstance instance = runtimeService().startProcessInstanceByKey("Process_TestProcess");
-        assertThat(instance).hasPassed("Activity_Test_01").isEnded();
+        ProcessInstance instance = runtimeService().startProcessInstanceByKey("Process_TestProcess", Map.of("myvar", 20L));
         assertThat(instance).variables().hasSize(1);
-        assertThat(instance).variables().containsEntry("my-var", 2L);
+        assertThat(instance).variables().containsEntry("myvar", 10L);
+        assertThat(instance).hasPassed("Activity_Test_01").isEnded().hasPassed("Event_PositiveValue");
+    }
+
+    @Test
+    @Deployment(resources = "test-process.bpmn")
+    public void testNegativeValue() {
+        IFn func = Clojure.var("clojure.core", "load-file");
+        func.invoke("delegate.clj");
+        ProcessInstance instance = runtimeService().startProcessInstanceByKey("Process_TestProcess", Map.of("myvar", 5L));
+        assertThat(instance).variables().hasSize(1);
+        assertThat(instance).variables().containsEntry("myvar", -5L);
+        assertThat(instance).hasPassed("Activity_Test_01").isEnded().hasPassed("Event_NegativeValue");
     }
 }
